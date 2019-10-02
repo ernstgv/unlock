66a982cd9d2022ef99abf4f2a760852a5ec416ff
Ernst Villareal <33861679+ernstgv@users.noreply.github.com>
2019-10-01 21:31:50 -0400

    Update unlock.py
    This will allow better run using pythonw. Logging using timestamp

--- unlock.py
+++ unlock.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 
-import pyautogui,time
+import pyautogui,time, sys
+from datetime import datetime
+
+pyautogui.FAILSAFE = False
+
+
 
 while True:
 
-    time.sleep(30)
+	timestamp = datetime.now()
+	#ranmessage = "I ran at "
+	message = "I ran at " + timestamp.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+	#print(timestamp)
+	unlocklog = open("C:/temp/unlock7logs/unlocklog.txt","w")
+	unlocklog.write(message)
+	unlocklog.close()
 
-    pyautogui.press('numlock')
+	time.sleep(480)
+
+	pyautogui.press('numlock')
+
+
+    #print("I kicked in")
