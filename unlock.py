9adacff0bb4b5f3096e447c26f6d6cf125a74035
Ernst Villareal <33861679+ernstgv@users.noreply.github.com>
2019-11-05 19:20:04 -0500

    Cleanup

--- unlock.py
+++ unlock.py
@@ -1,20 +1,18 @@
-#!/usr/bin/python
-
 import pyautogui,time, sys
 from datetime import datetime
 
 pyautogui.FAILSAFE = False
 
-
-
 while True:
 
 	timestamp = datetime.now()
-	#ranmessage = "I ran at "
+	
 	message = "I ran at " + timestamp.strftime("%d-%b-%Y (%H:%M:%S.%f)")
-	#print(timestamp)
-	unlocklog = open("C:/temp/unlock7logs/unlocklog.txt","w")
+	
+	unlocklog = open("C:/temp/unlock/unlocklog.txt","w")
+	
 	unlocklog.write(message)
+	
 	unlocklog.close()
 
 	time.sleep(480)
